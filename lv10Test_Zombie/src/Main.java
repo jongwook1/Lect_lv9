7287fd2812669d340f1f8d9c46762ae9b5b55c5f
A <A@112.171.219.179>
2021-10-20 15:20:51 +0900

    모든 기능완료

--- lv10Test_Zombie/src/Main.java
+++ lv10Test_Zombie/src/Main.java
@@ -2,7 +2,7 @@ import controller.Game;
 
 public class Main {
 //시작: 11:00
-//종료: 
+//종료: 15:20
 	public static void main(String[] args) {
 		Game.instance.run();
 
--- lv10Test_Zombie/src/models/Hero.java
+++ lv10Test_Zombie/src/models/Hero.java
@@ -2,7 +2,7 @@ package models;
 
 import controller.Game;
 
-public class Hero extends Unit{
+public class Hero extends Unit {
 
 	public Hero(String name, int hp, int att, int def, int pos) {
 		super(name, hp, att, def, pos);
@@ -20,14 +20,38 @@ public class Hero extends Unit{
 	}
 
 	public void attack(Unit enemy) {
-		if(enemy instanceof ZombieKing) {
-			
-		}else {
+		if (enemy instanceof ZombieKing) {
+			if (((ZombieKing) enemy).getShield() > 0) {
+				int deal = (this.getAtt() - enemy.getDef()) * (Game.rn.nextInt(150) + 50) / 100;
+				if (deal <= 0) {
+					deal = 1;
+				}
+				System.out.println(this.getName() + "의 공격");
+				System.out.println(enemy.getName() + "의 쉴드가 받은데미지: " + deal);
+				((ZombieKing) enemy).setShield(((ZombieKing) enemy).getShield() - deal);
+				if (((ZombieKing) enemy).getShield() <= 0) {
+					System.out.println(enemy.getName() + "의 쉴드가 깨졌습니다");
+					((ZombieKing) enemy).setShield(0);
+				}
+				System.out.println(enemy.getName() + "의 남은 체력 : " + enemy.getHp());
+			} else {
+				super.attack(enemy);
+			}
+
+		} else {
 			super.attack(enemy);
 		}
-		
-		
+
 	}
-	
-	
+
+	public void drink() {
+		if (this.getCnt() > 0) {
+			System.out.println("회복약을 마십니다");
+			System.out.println("체력100 회복");
+			this.setHp(this.getHp() + 100);
+			System.out.println(this.getName() + "의 남은체력: " + this.getHp());
+			this.setCnt(this.getCnt() - 1);
+		}
+	}
+
 }
